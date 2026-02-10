1343565c641032311c7e79c908fe518f26afedea
LukeByMark <lucademarco0406@gmail.com>
2026-02-10 11:51:02 +0100

    Fixed waiting enemy behavior

--- Assets/Scripts/WaitingEnemy.cs
+++ Assets/Scripts/WaitingEnemy.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 enum WaitingEnemyStates
@@ -12,16 +10,20 @@ enum WaitingEnemyStates
 public class WaitingEnemy : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
+    [SerializeField]
     private WaitingEnemyStates currentState;
     [SerializeField]
     private Vector2 originPosition;
     [SerializeField]
-    private Vector2 currentTarget;
+    private Transform currentTarget;
+    private float maxExposureTime = 1;
+    private bool isExposingPlayer = false;
 
     [SerializeField]
     public float RotationSpeed = 4.0f;
     public float ChaseSpeed = 4.0f;
     public float ReachDistance = 0.1f;
+    public float ExposureTime = 0;
 
     void Start()
     {
@@ -50,19 +52,22 @@ public class WaitingEnemy : MonoBehaviour
         switch (currentState)
         {
             case WaitingEnemyStates.Wait:
-                rigidbody.transform.Rotate(0, 0, RotationSpeed);
+                if (!isExposingPlayer)
+                {
+                    rigidbody.transform.Rotate(0, 0, RotationSpeed);
+                }
                 break;
             case WaitingEnemyStates.Chase:
                 if (currentTarget != null)
                 {
-                    Vector2 direction = (currentTarget - rigidbody.position).normalized;
+                    Vector2 direction = ((Vector2)currentTarget.position - rigidbody.position).normalized;
                     Vector2 velocity = direction * ChaseSpeed;
                     rigidbody.linearVelocity = velocity;
 
                     float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
                     transform.rotation = Quaternion.Euler(0, 0, angle);
                 }
-                if (Vector2.Distance(currentTarget, rigidbody.position) > 3.0f)
+                if (Vector2.Distance(currentTarget.position, rigidbody.position) > 3.0f)
                 {
                     Debug.Log("out of reach");
                     ChangeState(WaitingEnemyStates.Returning);
@@ -71,16 +76,14 @@ public class WaitingEnemy : MonoBehaviour
             case WaitingEnemyStates.Returning:
                 if (currentTarget != null)
                 {
-                    Vector2 direction = (currentTarget - rigidbody.position).normalized;
+                    Vector2 direction = ((Vector2)currentTarget.position - rigidbody.position).normalized;
                     Vector2 velocity = direction * ChaseSpeed;
                     rigidbody.linearVelocity = velocity;
 
                     float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
                     transform.rotation = Quaternion.Euler(0, 0, angle);
                 }
-                Debug.Log(Vector2.Distance(currentTarget, rigidbody.position));
-                Debug.Log(rigidbody.position);
-                if (Vector2.Distance(currentTarget, rigidbody.position) <= ReachDistance)
+                if (Vector2.Distance((Vector2)currentTarget.position, rigidbody.position) <= ReachDistance)
                 {
                     Debug.Log("change state");
                     ChangeState(WaitingEnemyStates.Wait);
@@ -96,9 +99,9 @@ public class WaitingEnemy : MonoBehaviour
             case WaitingEnemyStates.Wait:
                 break;
             case WaitingEnemyStates.Chase:
-                rigidbody.linearVelocity = Vector2.zero;
                 break;
             case WaitingEnemyStates.Returning:
+                rigidbody.linearVelocity = Vector2.zero;
                 break;
         }
 
@@ -110,15 +113,26 @@ public class WaitingEnemy : MonoBehaviour
             case WaitingEnemyStates.Chase:
                 break;
             case WaitingEnemyStates.Returning:
-                currentTarget = originPosition;
+                GameObject returnPosition = new GameObject("ReturnPosition");
+                returnPosition.transform.position = new Vector3(originPosition.x, originPosition.y, 0);
+                currentTarget = returnPosition.transform;
                 break;
         }
     }
     private void OnPlayerDetected(PlayerDetector detector, Transform playerTransform)
     {
+        if(ExposureTime < maxExposureTime)
+        {
+            ExposureTime += Time.deltaTime;
+        }
+        else
+        {
+            isExposingPlayer = true;
+        }
+
         if (currentState == WaitingEnemyStates.Wait && detector.GetComponentInParent<WaitingEnemy>() == this)
         {
-            currentTarget = playerTransform.position;
+            currentTarget = playerTransform;
             ChangeState(WaitingEnemyStates.Chase);
         }
     }
