613eebe88753ec27cadd52edb4563533c05a59fc
Alejandro <alex_belpo@hotmail.com>
2026-02-10 10:21:41 +0100

    Fixed PatrollingEnemy.cs

--- Assets/Scripts/PatrollingEnemy.cs
+++ Assets/Scripts/PatrollingEnemy.cs
@@ -1,8 +1,6 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem.LowLevel;
 
 enum PatrollingEnemyStates
 {
@@ -15,92 +13,74 @@ public class PatrollingEnemy : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
     private PatrollingEnemyStates currentState;
-    private int currentWayPoint;
-    private bool isMoving;
+    private Transform currentTarget;
 
     [SerializeField]
-    public List<GameObject> WayPoints;
-    public float Speed = 4.0f; 
+    public List<Transform> Waypoints;
+    public float PatrolSpeed = 2.0f;
+    public float ChaseSpeed = 3.0f;
+    public float ReachDistance = 0.1f;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         currentState = PatrollingEnemyStates.Patrol;
-        currentWayPoint = 0;
-        isMoving = false;
+        currentTarget = Waypoints.First();
+    }
+
+    private void OnEnable()
+    {
+        PlayerDetector.OnPlayerDetected += OnPlayerDetected;
+    }
+
+    private void OnDisable()
+    {
+        PlayerDetector.OnPlayerDetected -= OnPlayerDetected;
     }
 
     void FixedUpdate()
     {
-        GameObject targetWaypoint = WayPoints[currentWayPoint];
-        
-        if (!isMoving && targetWaypoint !=null)
+        if (currentTarget != null)
         {
-            Debug.Log("new waypoint");
-           Vector2 direction = ((Vector2)targetWaypoint.transform.position - rigidbody.position).normalized;
+            Vector2 direction = ((Vector2)currentTarget.position - rigidbody.position).normalized;
+            Vector2 velocity = direction * PatrolSpeed;
+            rigidbody.linearVelocity = velocity;
 
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
 
-            Vector2 velocity = direction * Speed;
-            rigidbody.linearVelocity = velocity;
+        switch (currentState)
+        {
+            case PatrollingEnemyStates.Patrol:
+                if (Vector2.Distance(currentTarget.position, rigidbody.position) <= ReachDistance)
+                {
+                    int currentIndex = Waypoints.IndexOf(currentTarget);
 
-<<<<<<< Updated upstream
-            isMoving = true;
-=======
                     currentIndex++;
                     if (currentIndex >= Waypoints.Count) currentIndex = 0;
+
                     currentTarget = Waypoints[currentIndex];
                 }
                 break;
             case PatrollingEnemyStates.Chase:
-                if (currentTarget != null)
-                {
-                    Vector2 direction = ((Vector2)currentTarget.position - rigidbody.position).normalized;
-                    Vector2 velocity = direction * PatrolSpeed;
-                    rigidbody.linearVelocity = velocity;
-
-                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                    transform.rotation = Quaternion.Euler(0, 0, angle);
-                }
-
                 if (Vector2.Distance(currentTarget.position, rigidbody.position) > 3.0f)
                 {
                     ChangeState(PatrollingEnemyStates.Returning);
                 }
                 break;
             case PatrollingEnemyStates.Returning:
-                if (currentTarget != null)
-                {
-                    Vector2 direction = ((Vector2)currentTarget.position - rigidbody.position).normalized;
-                    Vector2 velocity = direction * PatrolSpeed;
-                    rigidbody.linearVelocity = velocity;
-
-                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                    transform.rotation = Quaternion.Euler(0, 0, angle);
-                }
-
                 if (Vector2.Distance(currentTarget.position, rigidbody.position) <= ReachDistance)
                 {
                     ChangeState(PatrollingEnemyStates.Patrol);
                 }
                 break;
->>>>>>> Stashed changes
         }
-        Debug.Log(Vector2.Distance(targetWaypoint.transform.position, rigidbody.position));
-        if (isMoving && Vector2.Distance(targetWaypoint.transform.position, rigidbody.position)<= 0.10f)
-        {
-<<<<<<< Updated upstream
-            currentWayPoint++;
-            if (currentWayPoint >= WayPoints.Count)
-            {
-                currentWayPoint = 0;
-                
-            }
-            isMoving = false;
-        }
+    }
 
-=======
+    private void ChangeState(PatrollingEnemyStates newState) {
+        switch (currentState)
+        {
             case PatrollingEnemyStates.Patrol:
                 break;
             case PatrollingEnemyStates.Chase:
@@ -111,6 +91,7 @@ public class PatrollingEnemy : MonoBehaviour
         }
 
         currentState = newState;
+
         switch (currentState)
         {
             case PatrollingEnemyStates.Patrol:
@@ -133,6 +114,7 @@ public class PatrollingEnemy : MonoBehaviour
                 break;
         }
     }
+
     private void OnPlayerDetected(PlayerDetector detector, Transform playerTransform)
     {
         if (currentState != PatrollingEnemyStates.Chase && detector.GetComponentInParent<PatrollingEnemy>() == this)
@@ -140,6 +122,5 @@ public class PatrollingEnemy : MonoBehaviour
             currentTarget = playerTransform;
             ChangeState(PatrollingEnemyStates.Chase);
         }
->>>>>>> Stashed changes
     }
 }
